86d2801cba5ea2b450b32dd5bbb07b25fe33ced4
mikemasamvu <66001009+mikemasamvu@users.noreply.github.com>
2024-04-07 21:16:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function startDiscovering() {
+    // JavaScript to handle the "Start Discovering" action
+    // This could be a redirect, a function call, etc.
+    alert('Discovering started!');
+  }
+  
