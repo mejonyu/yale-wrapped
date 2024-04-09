2616ab24abd269f2dc4807cf80d7e8c6f25e83aa
mikemasamvu <66001009+mikemasamvu@users.noreply.github.com>
2024-04-09 11:37:06 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,64 @@
+// Home Page
 function startDiscovering() {
-    // JavaScript to handle the "Start Discovering" action
-    // This could be a redirect, a function call, etc.
-    alert('Discovering started!');
+    window.location.href = 'instructions.html';
   }
   
+// Instructions Page
+document.addEventListener('DOMContentLoaded', (event) => {
+  const ctaButton = document.querySelector('.cta-button');
+  
+  ctaButton.addEventListener('click', () => {
+    window.location.href = 'selection.html';
+  });
+});
+
+// Exit
+document.addEventListener('DOMContentLoaded', () => {
+  const circleElement = document.querySelector('.exit-footer');
+  circleElement.addEventListener('click', () => {
+    window.location.href = 'home.html';
+  });
+  // Placeholder for swipe left to return
+  document.querySelector('.option-card:nth-child(1)').addEventListener('click', () => {
+    alert('Swiped left to return!');
+  });
+
+  // Placeholder for swipe right for results
+  document.querySelector('.option-card:nth-child(2)').addEventListener('click', () => {
+    alert('Swiped right for results!');
+  });
+
+  // Placeholder for raise hand to exit
+  document.querySelector('.exit-button').addEventListener('click', () => {
+    window.location.href = 'home.html';
+  });
+});
+
+// Songs selection page
+document.addEventListener('DOMContentLoaded', () => {
+  const circleElement = document.querySelector('.circle');
+  circleElement.addEventListener('click', () => {
+    window.location.href = 'exit.html';
+  });
+
+  const instructionBox = document.querySelector('.instruction-box');
+  instructionBox.addEventListener('click', () => {
+    window.location.href = 'results.html';
+  });
+
+  const helpBox = document.querySelector('.help-box');
+  helpBox.addEventListener('click', () => {
+    window.location.href = 'instructions.html';
+  });
+
+  // Placeholder for swipe left to return
+  document.querySelector('.swipe-left').addEventListener('click', () => {
+    alert('Swiped left to return!');
+  });
+
+  // Placeholder for swipe right for results
+  document.querySelector('.swipe-right').addEventListener('click', () => {
+    alert('Swiped right for results!');
+  });
+
+});
