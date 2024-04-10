a31f29e66d1ff9130f93fe6701445ec59bdc5adb
Jonathan Yu <jonyu02@gmail.com>
2024-04-10 01:44:53 -0400

    Complete Spotify OAuth + redirect

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask
 from views import views
 
 app = Flask(__name__)
+
+# set the name of the session cookie
+app.config['SESSION_COOKIE_NAME'] = 'Spotify Cookie'
+
+# set a random secret key to sign the cookie
+app.secret_key = 'jfea849ap(*&*^$3)890$'
+
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,18 +1,33 @@
-from flask import Blueprint, render_template, redirect, request, session
+from flask import Blueprint, render_template, url_for, redirect, request, session
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 views = Blueprint(__name__, "views")
 
+# set the key for the token info in the session dictionary
+TOKEN_INFO = 'token_info'
+
 def create_spotify_oauth():
     return SpotifyOAuth(
         client_id = "90b2898c5e57448db258e1be673ea7b5",
         client_secret = "5a58c52be6604043b7db83785da05637",
-        redirect_uri = url_for('redirect_page', _external=True),
+        redirect_uri = url_for('views.redirect_page', _external=True),
         scope = "user-library-read playlist-modify-public playlist-modify-private"
     )
 
+# handle login
 @views.route("/")
-def home():
-    return render_template("index.html")
+def login():
+    auth_url = create_spotify_oauth().get_authorize_url()
+    # redirect to auth URL
+    return redirect(auth_url)
+
+@views.route("/redirect")
+def redirect_page():
+    pass
+
+
+# @views.route("/")
+# def home():
+#     return render_template("index.html")
