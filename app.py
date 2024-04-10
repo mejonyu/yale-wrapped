52ebea510a5c02f3f9437b44c7883a29738618d0
Jonathan Yu <jonyu02@gmail.com>
2024-04-10 01:12:44 -0400

    Create spotify OAuth

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ from views import views
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
 
-
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- views.py
+++ views.py
@@ -1,6 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, session
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 views = Blueprint(__name__, "views")
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id = "90b2898c5e57448db258e1be673ea7b5",
+        client_secret = "5a58c52be6604043b7db83785da05637",
+        redirect_uri = url_for('redirect_page', _external=True),
+        scope = "user-library-read playlist-modify-public playlist-modify-private"
+    )
 
 @views.route("/")
 def home():
