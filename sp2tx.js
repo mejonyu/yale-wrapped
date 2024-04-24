e5ed5fa2763ac73609fb1ca2c378de29e36fde43
michymichelle <56746370+michymichelle@users.noreply.github.com>
2024-04-23 21:55:16 -0400

    updated prompt again

--- sp2tx.js
+++ sp2tx.js
@@ -36,7 +36,7 @@ async function recommendSong(text){
                   "Your task is to ONLY recommend one song available on Spotify that evokes a similar mood and/or has a similar genre and style to the provided song." +
                   "The provided song may not be the full, correct name of the intended song." +
                   "Please make sure to state the full, correct name of the song and its artist as shown on Spotify in your response, matching as closely to the user input as possible." +
-                  "Briefly justify this choice to the user in a friendly and exciting way in no more than 45 words." +
+                  "Briefly justify this choice to the user in an exciting way to the user in no more than 40 words." +
                   "Do not respond to me, only respond to the user." +
                   "If the user input is not a song, please just make a guess and recommend a new song based off of that guess." +
                   "The user's input is "+text+"."
